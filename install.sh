b0828977818a508e4de8444de557cda72f0d3999
hostlinkcloud <studentapnar@gmail.com>
2026-02-20 11:57:43 +0600

    Change download source for bdix configuration files
    Updated download links for bdix.conf and bdix script.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ opkg update
 opkg install iptables iptables-mod-nat-extra redsocks
 
 #Then run this line
-service redsocks stop && mv /etc/redsocks.conf /etc/redsocks.conf.bkp && cd /etc && wget https://github.com/Sahosting/BdixBypass/raw/main/bdix.conf && mv /etc/init.d/redsocks /etc/init.d/redsocks.bkp && cd /etc/init.d && wget https://github.com/Sahosting/BdixBypass/raw/main/bdix && chmod +x /etc/init.d/bdix
+service redsocks stop && mv /etc/redsocks.conf /etc/redsocks.conf.bkp && cd /etc && wget https://github.com/hostlinkcloud/BdixBypass/raw/main/bdix.conf && mv /etc/init.d/redsocks /etc/init.d/redsocks.bkp && cd /etc/init.d && wget https://github.com/hostlinkcloud/BdixBypass/raw/main/bdix && chmod +x /etc/init.d/bdix
 
 cd /
 clear
